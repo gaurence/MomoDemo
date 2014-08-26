4563c5d66ca36fc42ec6fcc1faf874688eed9d43
kaideyi <kaideyi@163.com>
2014-08-26 21:41:09 +0800

    Initial Commit

---
+++ MomoDemoTests/MomoDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  MomoDemoTests.m
+//  MomoDemoTests
+//
+//  Created by kaideyi on 14-8-26.
+//  Copyright (c) 2014年 kaideyi.com. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MomoDemoTests : XCTestCase
+
+@end
+
+@implementation MomoDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
